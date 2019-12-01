180934e272e6e26afd8cffc2a8595e145470ac12
kmarks2013 <kmarks2013@gmail.com>
2019-12-01 17:14:36 -0500

    added hover wrapper and .hover and .trigger tags

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react'
 import TrigerTile from './Components/TrigerTile';
 import HoverTest from './Components/HoverTest';
-
+import ReactHover from 'react-hover'
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-
+      <ReactHover>
+      <ReactHover.Trigger>
         <TrigerTile/>
-   
-      
+      </ReactHover.Trigger>
+      <ReactHover.Hover>
         <HoverTest />
+      </ReactHover.Hover>
+
+      </ReactHover>
       </div>
     )
   }
