5b144164d2de4a7d99105a69cb64d80131038c9f
kmarks2013 <kmarks2013@gmail.com>
2019-12-01 16:04:05 -0500

    first commit

---
+++ src/Components/HoverTest.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class HoverTest extends Component {
+    render() {
+        return (
+            <div className='hover'>
+                <p> the rest of this sentence!</p>
+            </div>
+        )
+    }
+}
---
+++ src/Components/TrigerTile.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TrigerTile extends Component {
+    render() {
+        return (
+            <div className='trigger'>
+                <p> on Hover i should show </p>
+            </div>
+        )
+    }
+}
